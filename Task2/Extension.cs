d5725293f437d854a345cba86c557fa87646198e
Alexandr Sivohin <alexandrsivoh@gmail.com>
2021-03-18 02:50:29 +0300

    v 1.1, task2

---
+++ Task2/Extension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace Task2
+{
+    public static class Extension
+    {
+        public static long DirSize(DirectoryInfo d)
+        {
+            long size = 0;
+
+            FileInfo[] fis = d.GetFiles();
+            foreach (FileInfo f in fis)
+            {
+                size += f.Length;
+            }
+            DirectoryInfo[] dirN = d.GetDirectories();
+            foreach(DirectoryInfo di in dirN)
+            {
+                size += DirSize(di);
+            }
+            return size;
+        }
+    }
+}
--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task2
 {
@@ -6,7 +7,34 @@ namespace Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string path = Console.ReadLine();
+            DirectoryInfo dirInfo = new DirectoryInfo(path);
+            
+                var folders = dirInfo.GetDirectories();
+
+                WriteFolderInfo(folders);
+            
+            Console.ReadKey();
+        }
+
+        public static void WriteFolderInfo(DirectoryInfo[] folders)
+        {
+            Console.WriteLine("Folder:\n");
+
+            foreach(var folder in folders)
+            {
+                try
+                {
+                    Console.WriteLine(folder.Name + $" - {Extension.DirSize(folder)}байт");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(folder.Name + $" - невышло: ");
+
+                }
+                
+            }
         }
     }
 }
