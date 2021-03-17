30ad721bc44d4a1ca6e577bfd44c05a05f4afee4
Alexandr Sivohin <alexandrsivoh@gmail.com>
2021-03-17 21:38:42 +0300

    V 1.0

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace Task1
 {
@@ -6,7 +7,55 @@ namespace Task1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            try
+            {    
+                string path = Console.ReadLine();
+                DirectoryInfo dirInf = new DirectoryInfo(path);
+
+                if (dirInf.Exists)
+                {
+                    string[] dir = Directory.GetDirectories(path);
+                    string[] file = Directory.GetFiles(path);
+                    Console.WriteLine("\nDiriktory");
+                    Delete30min(dir);
+                    Console.WriteLine("\nFile");
+                    Delete30min(file);
+                }
+            else
+            {
+                Console.WriteLine("неправльно введен путь");
+            }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Возникла проблема:" + e.Message);
+            }
+            Console.ReadKey();
+        }
+        public static void Delete30min(string[] file)
+        {
+
+            foreach (string f in file)
+            {
+               var difTA = Directory.GetLastAccessTime(f);
+
+                TimeSpan interval = DateTime.Now - difTA;
+                Console.WriteLine(f);
+                Console.WriteLine("последнее использование: " + interval.TotalMinutes);
+
+                if (interval > TimeSpan.FromMinutes(30))
+                {
+                    FileInfo dirInfo = new FileInfo(f);
+                    dirInfo.Delete();
+                    Console.WriteLine("File delete");
+                }
+                else
+                {
+                    
+                    Console.WriteLine("this OK");
+                }
+            }
         }
     }
 }
--- WorkWithFile/Program.cs
+++ WorkWithFile/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace WorkWithFile
 {
@@ -6,7 +7,43 @@ namespace WorkWithFile
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Make sure a filename was provided.
+            if (args.Length > 0)
+            {
+                // Verify that the provided filename exists.
+                if (File.Exists(args[0]))
+                {
+                    FileInfo fi = new FileInfo(args[0]);
+                    touchFile(fi);
+                }
+                else
+                {
+                    Console.WriteLine(
+                        "Could not find the file: {0}.", args[0]);
+                }
+            }
+            else
+            {
+                Console.WriteLine("No file was specified.");
+            }
+            Console.ReadLine();
+        }
+
+        static void touchFile(FileSystemInfo fsi)
+        {
+            Console.WriteLine("Touching: {0}", fsi.FullName);
+
+            // Update the CreationTime, LastWriteTime and LastAccessTime.
+            try
+            {
+                fsi.CreationTime = fsi.LastWriteTime = fsi.LastAccessTime =
+                    DateTime.Now;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+            }
         }
     }
 }
+
