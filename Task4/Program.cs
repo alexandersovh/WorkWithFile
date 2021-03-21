d4418ba563f5b4f879dd5671e855ab168c111282
Alexandr Sivohin <alexandrsivoh@gmail.com>
2021-03-21 21:31:58 +0300

    v1.4.1

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -6,28 +6,53 @@ namespace FinalTask
 {
     class Program
     {
-        const string Path = @"C:\Users\alexandr\Desktop\Students.dat";
+        const string CreateFolder = "Students";
+        const string PathDesctop = @"C:\Users\alexandr\Desktop\";
         static void Main(string[] args)
         {
-            try
+
+            SortStudentToFile(PathDesctop);
+            Console.ReadKey();
+        }
+
+        /// <summary>
+        /// Дисериализация файла
+        /// </summary>
+        /// <returns> массив студентов </returns>
+        static Student[] ReadFile(string peth)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fs = new FileStream(peth, FileMode.Open))
             {
-                BinaryFormatter formatter = new BinaryFormatter();
-                using (var fs = new FileStream(Path, FileMode.Open))
+                var newStud = (Student[])formatter.Deserialize(fs);
+                return newStud;
+            }
+        }
+        static void SortStudentToFile(string path)
+        {
+            
+            Student[] stud = ReadFile(path + "Students.dat");
+            Directory.CreateDirectory(path + CreateFolder);
+
+            foreach (Student arrGrop in stud)
+            {
+                string studGroup = Path.Combine(path,CreateFolder,arrGrop.Group + ".txt");
+                if (!File.Exists(studGroup)) 
                 {
-                    var newStud = (Student[])formatter.Deserialize(fs);
-                    foreach (var s in newStud)
+                    using (StreamWriter sw = File.CreateText(studGroup))
                     {
-                        Console.WriteLine($"Group: {s.Group}; Имя: {s.Name}; Возраст: {s.DateOfBirth}");
+                        foreach(Student arrStud in stud)
+                        {
+                            if (arrStud.Group == arrGrop.Group)
+                            {
+                                sw.WriteLine($"Имя: {arrStud.Name}, Дата раждения: {arrStud.DateOfBirth.Date}," +
+                                    $" их группа,{arrStud.Group} \n________________________________________________");
+                            }
+                        }
                     }
                 }
-                Console.ReadLine();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Ошибка: " + e.Message);
-            }
 
-            Console.ReadKey();
+            }
         }
     }
 }
