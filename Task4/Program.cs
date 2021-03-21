739858a7cea108dd43d04c3c8cc868ef8b9e4ee9
Alexandr Sivohin <alexandrsivoh@gmail.com>
2021-03-21 15:23:40 +0300

    v1.3.1 вывел даннвые

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
-namespace Task4
+namespace FinalTask
 {
     class Program
     {
+        const string Path = @"C:\Users\alexandr\Desktop\Students.dat";
         static void Main(string[] args)
         {
-            Studetns.WriteData();
+            try
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                using (var fs = new FileStream(Path, FileMode.Open))
+                {
+                    var newStud = (Student[])formatter.Deserialize(fs);
+                    foreach (var s in newStud)
+                    {
+                        Console.WriteLine($"Group: {s.Group}; Имя: {s.Name}; Возраст: {s.DateOfBirth}");
+                    }
+                }
+                Console.ReadLine();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Ошибка: " + e.Message);
+            }
+
             Console.ReadKey();
         }
     }
--- Task4/StudList.cs
+++ Task4/StudList.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
-using System.Collections.Generic;
-using System.Text;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace Task1_clin
+namespace FinalTask
 {
     [Serializable]
-    class Template
+    class Student
     {
         public string Name { get; set; }
         public string Group { get; set; }
         public DateTime DateOfBirth { get; set; }
-        public Template(string name, string group, DateTime dateOfBirth)
+        public Student(string name, string group, DateTime dateOfBirth)
         {
             Name = name;
             Group = group;
